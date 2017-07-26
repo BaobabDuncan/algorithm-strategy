0e9407f70cc0850525d80701cfca56ea8055324c
BaobabDuncan <baobab.duncan@gmail.com>
2017-07-26 10:58:00 +0900

    section 01
    - decimal number to hexadecimal
    - hexadecimal to decimal number

--- basic/01/main.c
+++ basic/01/main.c
@@ -1,6 +1,19 @@
 #include "stdio.h"
 
 int main() {
-    printf("main run!!");
+    char key;
+    int data;
+
+    scanf("%c", &key);
+
+    if (key == 'a') {
+        scanf("%d", &data);
+        printf("%d -> %x\n", data, data);
+    } else if (key == 'b') {
+        scanf("%x", &data);
+        printf("%x -> %d\n", data, data);
+    } else {
+        printf("wrong!!!\n");
+    }
     return 0;
 }
